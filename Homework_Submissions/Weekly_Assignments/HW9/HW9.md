03ad65b91582d1c2484a790db0755a7170b5e872
porter6553 <laurenporter@arizona.edu>
2023-10-30 14:44:37 -0500

    Week 9 (or 10) assignment
    Week 9 or 10 assignment

---
+++ Homework_Submissions/Weekly_Assignments/HW9/HW9.md
@@ -0,0 +1,21 @@
+# Week 9 Assignment
+Timeseries and Functions
+
+## Forecast Summary 
+1 Week Forecast = 108.3103448275862
+2 Week Forecast = 105.3103448275862
+
+I calculated this forecast by taking the average of the month of October flow. In order to get a more accurate forecast, I looked more specifically at the last weeks forecast and I have noticed an upward trend in flow over the last week into the 110's. This helped me to decide how much I would add to the flow. There is no rainfall forecasted in the area over the next two weeks so I tried to have my forecast reflect that. 
+
+## Script Improvements
+This week, I made my script easier to understand by using comments and more direct naming of different variables. I did my best to make the changes suggested by flake 8, but some of them are a bit difficult to figure out what they mean. I also added in a function and changed the fontsize of the graphs titles so they were easier to read.
+
+## Function
+I added a function in to define the month and dataframe I wanted to use for my timeseries. It was used to specify a year of 2023 and any flow that has occured including or after October. I added this function because it made the most sense for what we have discussed in class and it made understanding how I got just october a little easier.
+
+## Graph
+![Plot](Plot.png)
+I was able to make it work and look pretty! Thanks for your help!
+
+## Remaining Questions
+I think my script makes far more sense to me now and I have been able to gain a better understanding after our office hours. I am really looking forward to getting the extra practice time in class this week.
---
+++ Homework_Submissions/Weekly_Assignments/HW9/week9_functions.py
@@ -0,0 +1,69 @@
+# Starter code for week 9 Timeseries and Functions
+
+# %%
+# Import the modules we will use
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %%
+# ** MODIFY **
+# Set the file name and path to where you have stored the data
+filename = 'streamflow_week9.txt'
+filepath = os.path.join('../../../data', filename)
+print(os.getcwd())
+print(filepath)
+
+# %%
+# Read the data into a pandas dataframe
+dataframe = pd.read_table(filepath, sep='\t', skiprows=30,
+                    names =['agency_cd', 'site_no',
+                            'datetime', 'flow', 'code'],
+                            parse_dates =['datetime']
+                    )
+
+print(dataframe)
+
+# %%
+# Convert the dataframe into an indexed dataframe and set 
+# parameters for what to keep in the dataframe for your calculation. 
+# This is how to do it without creating a function
+dataframe_index=dataframe.set_index('datetime')
+oct = (dataframe_index.flow[(dataframe_index.index.month >= 10) & (dataframe_index.index.year == 2023)])
+oct1 = oct.mean()
+print(oct1)
+
+#%%
+# Create a function that will allow user to specify the month and year needed
+# for the calculation of this weeks streamflow forecast.
+# This does the same thing as above, but in the format of a function.
+def monthly(dataframe, month):
+    oct = (dataframe.flow[(dataframe.index.month >= month) & (dataframe.index.year == 2023)])
+    return(oct)
+my_output = monthly(dataframe = dataframe_index, month = 10)
+print(my_output.mean())
+
+# %%
+# The ouput for the one week and two week streamflow forecast.
+week1 = (my_output.mean()) + 13
+print("1 Week Forecast =", week1)
+week2 = (my_output.mean()) + 10
+print("2 Week Forecast =", week2)
+
+# %%
+# Plot a figure with two plots, one showing the month of October 2023,
+# the other showing the week one and two streamflow forecast in bar graph form.
+fig, ax = plt.subplots(2,1)
+fig.set_size_inches(20,20)
+ax[0].plot(oct)
+ax[0].tick_params(axis='x', labelrotation=45)
+ax[1].bar(x=['week1', 'week2'], height=[week1, week2])
+ax[0].set_title('Month of October, 2023 Flow')
+ax[0].title.set_size(25)
+ax[1].set_title('Week 1 and Week 2 Forecast')
+ax[1].title.set_size(25)
+ax[0].set_ylabel('flow')
+ax[1].set_ylabel('flow')
+ax[0].set_xlabel('Date')
+ax[1].set_xlabel('Week')
+# %%
